03648ad1331b17ecb6af447757ca78342ea402a9
Kleis Auke Wolthuizen <github@kleisauke.nl>
2026-08-16 14:40:24 +0200

    Sync `app.{c,h}`

--- src/app.h
+++ src/app.h
@@ -1,11 +1,11 @@
 #ifndef __APP_H
 #define __APP_H
 
-#define APP_TYPE (vipsdisp_app_get_type())
-G_DECLARE_FINAL_TYPE(VipsdispApp, vipsdisp_app, VIPSDISP, APP, GtkApplication)
-
+#define APP_TYPE (app_get_type())
 #define APP VIPSDISP_APP
 
-VipsdispApp *vipsdisp_app_new(void);
+G_DECLARE_FINAL_TYPE(App, app, VIPSDISP, APP, GtkApplication)
+
+App *app_new(void);
 
 #endif /*__APP_H*/
